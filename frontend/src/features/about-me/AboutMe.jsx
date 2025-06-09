7102f2c0e09445ee8d72a84d72b70ae78a44c0a0
Maha Uddin <Student@Admins-MacBook-Pro.local>
2025-06-08 22:58:02 -0400

    updated

---
+++ frontend/src/features/about-me/AboutMe.jsx
@@ -0,0 +1,9 @@
+function AboutMe() {
+    return (
+      <div>
+        <h2>Mobile App Developer</h2>
+        <div>Delete this function and implement your code, Ali</div>
+      </div>
+    );
+  }
+  export default AboutMe;
